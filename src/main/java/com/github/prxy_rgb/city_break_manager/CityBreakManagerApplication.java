588bc1aefd754ca066ca46575084112fad7c23e5
Pradeep Ram V <prxy1712@gmail.com>
2025-10-21 17:12:25 +0530

    first commit

---
+++ src/main/java/com/github/prxy_rgb/city_break_manager/CityBreakManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.prxy_rgb.city_break_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBreakManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBreakManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/prxy_rgb/city_break_manager/CityBreakManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prxy_rgb.city_break_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBreakManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
